b3b8c17dbf227c0e79543f83a41cccae0adb0d70
EmptyDreadbot <mason.hayes@dexterschools.org>
2026-01-23 15:08:40 -0500

    first test

--- src/main/java/frc/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -1,5 +1,77 @@
 package frc.robot.subsystems.turret;
 
-public class Turret {
+import org.littletonrobotics.junction.Logger;
+import edu.wpi.first.math.MathUtil;
+import java.util.function.DoubleSupplier;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class Turret extends SubsystemBase {
     
+    private TurretIOInputsAutoLogged inputs = new TurretIOInputsAutoLogged();
+    private TurretIO io;
+    private final PIDController pid = new PIDController(0.0, 0.0, 0);
+    private final ArmFeedforward feedforward = new ArmFeedforward(0.00, 0.00, 0.00);
+    private final TrapezoidProfile profile = new TrapezoidProfile(new TrapezoidProfile.Constraints(0, 0));
+    private TrapezoidProfile.State goal = new TrapezoidProfile.State();
+    private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
+    public DoubleSupplier joystickOverride;
+    public double voltage;
+
+
+    public Turret(TurretIO io) {
+        this.io = io;
+        this.joystickOverride = () -> 0.0;
+        this.voltage = 0;
+        io.updateInputs(inputs);
+        goal = new TrapezoidProfile.State(inputs.rotationDegrees, 0);
+        setpoint = goal;
+    }
+
+    @Override
+    public void periodic() {
+        io.updateInputs(inputs);
+        Logger.processInputs("Wrist", inputs);
+
+        if (DriverStation.isDisabled()) {
+            setpoint = new TrapezoidProfile.State(inputs.rotationDegrees, 0);
+            goal = setpoint;
+        }
+
+        Logger.recordOutput("Turret/SetpointPosition", setpoint.position);
+        Logger.recordOutput("Turret/GoalPosition", goal.position);
+        Logger.recordOutput("Turret/AtSetpoint", atSetpoint());
+        setpoint = profile.calculate(0.02, setpoint, goal);
+        voltage = pid.calculate(inputs.rotationDegrees, setpoint.position)
+        + feedforward.calculate(Units.degreesToRadians(setpoint.position) ,setpoint.velocity);
+
+    }   
+    
+    public Command setAngleDegrees(double angle) {
+        return runOnce(
+            () -> {
+                goal = new TrapezoidProfile.State(angle, 0);
+            } );
+    }
+
+    public Command setAtZero() {
+        return runOnce(
+            () -> {
+
+            } );
+    }
+
+    public double getAngle() {
+        return inputs.rotationDegrees;
+    }
+
+    public boolean atSetpoint() {
+        return MathUtil.isNear(goal.position, inputs.rotationDegrees, 0.0);
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/turret/TurretIO.java
@@ -0,0 +1,36 @@
+package frc.robot.subsystems.turret;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+public interface TurretIO {
+    @AutoLog
+    public static class TurretIOInputs {
+         //The Volts
+        public double pivotAppliedVolts = 0.0;
+
+        //Current
+        public double pivotCurrentAmps = 0.0;
+
+        //Rotating By Degrees
+        public double pivotRotationDegrees = 0.0;
+        
+        //RPM
+        public double intakeRPM = 0.0;
+        public double absolutePosition = 0.0;
+
+        //Temp
+        public double pivotTemperature = 0.0;
+    }
+
+    public default void updateInputs(TurretIOInputs inputs) {};
+
+    public default void runPivotVoltage(double volts) {}
+
+    public default void runIntakeVoltage(double volts) {}
+
+    public default void setIdleMode(IdleMode pivotIdleMode, IdleMode intakeIdleMode) {};
+
+    public default void stopMotors() {};
+}
