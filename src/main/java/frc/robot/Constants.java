c767a49cbafba3322c445f6998c8640b6b6bd96a
codperson-cyber <liambreinert@gmail.com>
2026-01-24 13:03:58 -0500

    indexer SPARKFLEX created with Constants.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.RobotBase;
+
+public class Constants {
+    public static class IndexerConstants {
+        // public static final double INTAKE_VOLTAGE = 1.5;
+        // public static final double OUTAKE_VOLTAGE = -5.0;
+        public static final int MOTOR_ID = 1;
+    }
+}
