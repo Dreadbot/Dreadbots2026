17cd35eae28b3ce18d5d03d4ca106973253ba2dc
Mason-1473 <masonthayes@gmail.com>
2026-01-27 15:45:54 -0500

    fixing up gradle

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "d99ad150e81ad64fc372313ac40b1602d205368e";
-  public static final String GIT_DATE = "2026-01-19 21:28:23 EST";
+  public static final int GIT_REVISION = 7;
+  public static final String GIT_SHA = "545bbc08095ba414a63839041f2d95a105d603a1";
+  public static final String GIT_DATE = "2026-01-24 14:26:52 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-24 11:59:46 EST";
-  public static final long BUILD_UNIX_TIME = 1769273986490L;
+  public static final String BUILD_DATE = "2026-01-26 16:43:56 EST";
+  public static final long BUILD_UNIX_TIME = 1769463836155L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
