0dcb3d30d669bc3f673aabaa4bb4a38db8d214b2
Mason-1473 <masonthayes@gmail.com>
2026-01-28 12:58:32 -0500

    added vision code and fixed robot + robot container

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "545bbc08095ba414a63839041f2d95a105d603a1";
-  public static final String GIT_DATE = "2026-01-24 14:26:52 EST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "7b13ffa48be066b1546d17fa83b739acae12826e";
+  public static final String GIT_DATE = "2026-01-27 18:12:48 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-24 11:59:46 EST";
-  public static final long BUILD_UNIX_TIME = 1769273986490L;
+  public static final String BUILD_DATE = "2026-01-28 12:56:56 EST";
+  public static final long BUILD_UNIX_TIME = 1769623016280L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
---
+++ src/main/java/frc/robot/subsystems/vision/VisionIOCamera.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems.vision;
+
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.PubSubOption;
+import edu.wpi.first.networktables.StructArraySubscriber;
+import frc.robot.util.vision.VisionPosition;
+
+public class VisionIOCamera implements VisionIO{
+    private StructArraySubscriber<VisionPosition> visionPositions;
+    private DoubleSupplier latency;
+
+    public VisionIOCamera(String tableName) {
+        NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
+        NetworkTable visionTable = ntinst.getTable(tableName);
+        this.visionPositions = visionTable.getStructArrayTopic("visionPos", VisionPosition.struct).subscribe(
+            new VisionPosition[]{}, 
+            PubSubOption.periodic(0.02), 
+            PubSubOption.sendAll(true));
+        this.latency = visionTable.getDoubleTopic("visionLatency").subscribe(
+            0.0, 
+            PubSubOption.periodic(0.02), 
+            PubSubOption.sendAll(true));
+    }
+
+    @Override
+    public void updateInputs(VisionIOInputs inputs) {
+        VisionPosition[] currentPositions = visionPositions.get();
+        VisionDetection[] tmp = new VisionDetection[currentPositions.length];
+        if(currentPositions.length > 0) {
+            for (var i = 0; i < currentPositions.length; i++) {
+                var currentPosition = currentPositions[i];
+                tmp[i] = new VisionDetection(
+                    new Pose2d(
+                        currentPosition.x, 
+                        currentPosition.y, 
+                        Rotation2d.fromRadians(currentPosition.r)), 
+                    currentPosition.ID, 
+                    visionPositions.getAtomic().timestamp);
+            }
+        }
+        inputs.detections = tmp;
+        inputs.visionDelay = latency.getAsDouble();
+    }
+}
