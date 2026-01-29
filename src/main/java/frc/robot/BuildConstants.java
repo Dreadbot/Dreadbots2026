18ffd4d69e2f0a9f008177bb5b46d8f15f44975d
Low-Debug <Landon.stromain@gmail.com>
2026-01-29 15:47:25 -0500

    Climb base setup?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "1096f0755b9c39fc7506f86ff293736bff7c3a87";
-  public static final String GIT_DATE = "2026-01-24 20:24:09 EST";
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "2d78e11b17597624f6896b33b2d31056591d8ae2";
+  public static final String GIT_DATE = "2026-01-27 18:02:16 EST";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2026-01-27 17:34:51 EST";
-  public static final long BUILD_UNIX_TIME = 1769553291980L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-01-29 15:42:08 EST";
+  public static final long BUILD_UNIX_TIME = 1769719328629L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
---
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems.climb;
+
+import com.revrobotics.sim.SparkFlexSim;
+import com.revrobotics.spark.SparkFlex;
+
+
+//Redone Code from 2024/2025 Slapdown Algae I wrote to get voltage from a motor needs work so commented out for now -Landon
+
+// I think issue is that the Sparkflex Motor has not been defined nad named yet 
+public class ClimbIOSim implements ClimbIO {
+
+ ///private final SparkFlex ScrewMotor;
+
+
+//  public ClimbIOSim() {
+        
+    //      ScrewMotorVolts = 0.0;
+
+    // private double ScrewMotorVolts;
+ 
+    // @Override
+    // public void updateInputs(ClimbIOInputs inputs) {
+      
+
+    //     inputs.screwMotorAppliedVolts = ScrewMotorVolts;
+        
+       
+    //     inputs.screwMotorCurrentAmps = ScrewMotor.getCurrentDrawAmps();
+
+       //}  
+    //}
+}
