b014574bb2ef2866e4fa50d00adb411b6a7f04ce
Low-Debug <Landon.stromain@gmail.com>
2026-01-29 17:00:54 -0500

    Climb Sub Branch builded self

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 29;
-  public static final String GIT_SHA = "32954ae8d7dd0db2806a98c483d4a8a52b40c5ee";
-  public static final String GIT_DATE = "2026-01-29 16:11:09 EST";
-  public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2026-01-29 16:14:24 EST";
-  public static final long BUILD_UNIX_TIME = 1769721264826L;
+  public static final int GIT_REVISION = 30;
+  public static final String GIT_SHA = "4d049e5a0c43da59601dfb122b43157d400fac30";
+  public static final String GIT_DATE = "2026-01-29 16:16:57 EST";
+  public static final String GIT_BRANCH = "Climb";
+  public static final String BUILD_DATE = "2026-01-29 16:59:01 EST";
+  public static final long BUILD_UNIX_TIME = 1769723941311L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -1,5 +1,26 @@
 package frc.robot.subsystems.climb;
 
-public class Climb {
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Climb  extends SubsystemBase {
     
+    private ClimbIO io;
+    private ClimbIOInputsAutoLogged inputs = new ClimbIOInputsAutoLogged();
+    
+    @AutoLogOutput
+    public boolean isClimbed = false; 
+    public Climb(ClimbIO io) { 
+        this.io = io;
+}
+    @Override
+    public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Climb", inputs);
+   
+    }
 }
