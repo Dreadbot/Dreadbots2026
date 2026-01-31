dd5e019d09c03bdf369b83ff2a0f34f5acf998f6
codperson-cyber <liambreinert@gmail.com>
2026-01-31 11:42:55 -0500

    formatting RobotContainer.java and adding indexer to secondary controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -24,6 +24,10 @@ import frc.robot.subsystems.vision.VisionIO;
 import frc.robot.subsystems.vision.VisionIOCamera;
 
 import frc.robot.subsystems.flywheel.*;
+import frc.robot.subsystems.indexer.Indexer;
+import frc.robot.subsystems.indexer.IndexerIO;
+import frc.robot.subsystems.indexer.IndexerIOSim;
+import frc.robot.subsystems.indexer.IndexerIOSparkFlex;
 
 public class RobotContainer {
 
@@ -33,114 +37,126 @@ public class RobotContainer {
     private final Vision vision;
     private final List<VisionCamera> cameras;
     private final Flywheel flywheel;
+    private Indexer indexer;
 
     public RobotContainer() {
         switch (Constants.currentMode) {
-                case REAL: 
+            case REAL:
 
-                drive = 
-                new Drive(
-                    new GyroIONavX(),
-                    new ModuleIOSpark(0),
-                    new ModuleIOSpark(1),
-                    new ModuleIOSpark(2),
-                    new ModuleIOSpark(3));
+                drive = new Drive(
+                        new GyroIONavX(),
+                        new ModuleIOSpark(0),
+                        new ModuleIOSpark(1),
+                        new ModuleIOSpark(2),
+                        new ModuleIOSpark(3));
+                indexer = new Indexer(new IndexerIOSparkFlex());
                 // turret = new Turret(new TurretIOSparkMax());
                 cameras = List.of(
-            new VisionCamera(
-            new VisionIOCamera(VisionConstants.frontRightCameraName), 
-            0),
-            new VisionCamera(
-            new VisionIOCamera(VisionConstants.frontLeftCameraName),
-            1),
-            new VisionCamera(
-            new VisionIOCamera(VisionConstants.backCameraName),
-            2));
-        vision = new Vision(
-            cameras,
-            drive::addVisionMeasurement,
-            drive::getPose);
+                        new VisionCamera(
+                                new VisionIOCamera(VisionConstants.frontRightCameraName),
+                                0),
+                        new VisionCamera(
+                                new VisionIOCamera(VisionConstants.frontLeftCameraName),
+                                1),
+                        new VisionCamera(
+                                new VisionIOCamera(VisionConstants.backCameraName),
+                                2));
+                vision = new Vision(
+                        cameras,
+                        drive::addVisionMeasurement,
+                        drive::getPose);
                 CameraServer.startAutomaticCapture(0);
 
                 flywheel = new Flywheel(new FlywheelIOSparkFlex());
                 break;
-                case SIM:
+            case SIM:
 
-                drive = 
-                new Drive(
-                    new GyroIO() {},
-                    new ModuleIOSim(),
-                    new ModuleIOSim(),
-                    new ModuleIOSim(),
-                    new ModuleIOSim());
-                    cameras = List.of(
-            new VisionCamera(
-                new VisionIOCamera(VisionConstants.frontRightCameraName), 
-                0),
-            new VisionCamera(
-                new VisionIOCamera(VisionConstants.frontLeftCameraName),
-                1),
-            new VisionCamera(
-                new VisionIOCamera(VisionConstants.backCameraName),
-                2));
-            vision = new Vision(
-            cameras,
-            drive::addVisionMeasurement,
-            drive::getPose);
+                drive = new Drive(
+                        new GyroIO() {
+                        },
+                        new ModuleIOSim(),
+                        new ModuleIOSim(),
+                        new ModuleIOSim(),
+                        new ModuleIOSim());
+                indexer = new Indexer(new IndexerIOSim());
+                cameras = List.of(
+                        new VisionCamera(
+                                new VisionIOCamera(VisionConstants.frontRightCameraName),
+                                0),
+                        new VisionCamera(
+                                new VisionIOCamera(VisionConstants.frontLeftCameraName),
+                                1),
+                        new VisionCamera(
+                                new VisionIOCamera(VisionConstants.backCameraName),
+                                2));
+                vision = new Vision(
+                        cameras,
+                        drive::addVisionMeasurement,
+                        drive::getPose);
 
                 flywheel = new Flywheel(new FlywheelIOSim());
                 // turret = new Turret(new TurretIOSim());
                 break;
 
-                default:
+            default:
 
-                drive = 
-                    new Drive(
-                        new GyroIO() {},
-                        new ModuleIO() {}, 
-                        new ModuleIO() {}, 
-                        new ModuleIO() {}, 
-                        new ModuleIO() {});
-
-                        cameras = List.of(
-            new VisionCamera(
-                new VisionIO() {},
-                0),
-            new VisionCamera(
-                new VisionIO() {},
-                1),
-            new VisionCamera(
-                new VisionIO() {},
-                2));
-            vision = new Vision(
-            cameras,
-            drive::addVisionMeasurement,
-            drive::getPose);
+                drive = new Drive(
+                        new GyroIO() {
+                        },
+                        new ModuleIO() {
+                        },
+                        new ModuleIO() {
+                        },
+                        new ModuleIO() {
+                        },
+                        new ModuleIO() {
+                        });
+                indexer = new Indexer(new IndexerIO() {
+                });
+                cameras = List.of(
+                        new VisionCamera(
+                                new VisionIO() {
+                                },
+                                0),
+                        new VisionCamera(
+                                new VisionIO() {
+                                },
+                                1),
+                        new VisionCamera(
+                                new VisionIO() {
+                                },
+                                2));
+                vision = new Vision(
+                        cameras,
+                        drive::addVisionMeasurement,
+                        drive::getPose);
 
                 flywheel = new Flywheel(new FlywheelIOSim());
                 // turret = new Turret(new TurretIO() {});
                 break;
-            }
-            configureButtonBindings();
         }
+        configureButtonBindings();
+    }
 
-private void configureButtonBindings() {
+    private void configureButtonBindings() {
         VisionUtil.getApriltagPose(1);
-         drive.setDefaultCommand(
-              DriveCommands.joystickDrive(
-                  drive, 
-                      () -> -primaryController.getLeftY(),
-                      () -> -primaryController.getLeftX(),
-                      () -> -primaryController.getRightX()));
+        drive.setDefaultCommand(
+                DriveCommands.joystickDrive(
+                        drive,
+                        () -> -primaryController.getLeftY(),
+                        () -> -primaryController.getLeftX(),
+                        () -> -primaryController.getRightX()));
 
-          primaryController.start().onTrue(
-              Commands.runOnce(
-                  () -> drive.setPose(
-                          new Pose2d(vision.getLastVisionPose().getTranslation(), new Rotation2d())),
-                          drive).ignoringDisable(true));
-         }
-         public Command getAutonomousCommand() {
-        return null; //choreoAutoChooser.selectedCommand();
+        primaryController.start().onTrue(
+                Commands.runOnce(
+                        () -> drive.setPose(
+                                new Pose2d(vision.getLastVisionPose().getTranslation(), new Rotation2d())),
+                        drive).ignoringDisable(true));
+        secondaryController.leftTrigger().onTrue(indexer.intake());
+    }
+
+    public Command getAutonomousCommand() {
+        return null; // choreoAutoChooser.selectedCommand();
     }
 
     public void autonomousInit() {
