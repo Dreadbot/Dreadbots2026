d81de0dec61ba62363646b73f68b2490d045110c
jeejDev <156706451+Butter-Stik@users.noreply.github.com>
2026-02-06 16:57:41 -0500

    5m/s^2 auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "94ce84ce5740f83da673dda9e20b562d2a0eb9e0";
-  public static final String GIT_DATE = "2026-01-31 12:53:32 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "d3eb9f8ce53d9c5f529c4496bcd7ccf33159fede";
+  public static final String GIT_DATE = "2026-02-06 16:01:50 EST";
   public static final String GIT_BRANCH = "vision-bot";
-  public static final String BUILD_DATE = "2026-02-05 18:36:16 EST";
-  public static final long BUILD_UNIX_TIME = 1770334576317L;
+  public static final String BUILD_DATE = "2026-02-06 16:51:53 EST";
+  public static final long BUILD_UNIX_TIME = 1770414713184L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
