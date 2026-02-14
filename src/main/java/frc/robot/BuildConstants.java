53275a78af6484f4c565d3eb02148b5b707237a6
joshuaharding2 <joshuaharding248@gmail.com>
2026-02-14 10:36:21 -0500

    Tested it and it works

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 53;
-  public static final String GIT_SHA = "f3aa2e3bd3ab709843f75c854d6344f4c4c38ab9";
-  public static final String GIT_DATE = "2026-02-13 17:56:04 EST";
+  public static final int GIT_REVISION = 55;
+  public static final String GIT_SHA = "76716b936c36186485865f4bedab6cfccbfe6742";
+  public static final String GIT_DATE = "2026-02-14 09:51:24 EST";
   public static final String GIT_BRANCH = "sparkMax-joystick";
-  public static final String BUILD_DATE = "2026-02-13 18:11:42 EST";
-  public static final long BUILD_UNIX_TIME = 1771024302620L;
+  public static final String BUILD_DATE = "2026-02-14 10:32:53 EST";
+  public static final long BUILD_UNIX_TIME = 1771083173867L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
