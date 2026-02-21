246a0df9928af28cc452f16580e40d646625666e
joshuaharding2 <joshuaharding248@gmail.com>
2026-02-21 11:50:45 -0500

    Revert "Merge branch 'Climb-Servo' into climb-commands" because apparently it isn't needed after all
    This reverts commit 2ae46ab21af177a0868f4621f4dab771881a081c, reversing
    changes made to 58c2473fa6f020fd24c335c7964bc996ea7455c8.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -35,7 +35,6 @@ import frc.robot.subsystems.climb.*;
 import frc.robot.subsystems.flywheel.*;
 import frc.robot.subsystems.hood.*;
 import frc.robot.subsystems.indexer.*;
-import frc.robot.subsystems.hookprep.*;
 
 public class RobotContainer {
 
@@ -50,7 +49,6 @@ public class RobotContainer {
     // private final Indexer indexer;
     // private final AutoAim autoAim;
     private final Climb climb;
-    // private final Hookprep hookprep;
 
     public RobotContainer() {
         switch (Constants.currentMode) {
@@ -74,7 +72,6 @@ public class RobotContainer {
                 // hood = new Hood(new HoodIOSparkMax());
                 // indexer = new Indexer(new IndexerIOSparkFlex());
                 climb = new Climb(new ClimbIOSparkFlex());
-                // hookprep = new Hookprep(new HookprepIOServo() {});
                 break;
             case SIM:
                 // drive = new Drive(
@@ -95,7 +92,6 @@ public class RobotContainer {
                 // hood = new Hood(new HoodIOSim());
                 // indexer = new Indexer(new IndexerIOSim());
                 climb = new Climb(new ClimbIOSim());
-                // hookprep = new Hookprep(new HookprepIOSim() {});
                 break;
             default:
                 // drive = new Drive(
@@ -116,7 +112,6 @@ public class RobotContainer {
                 // hood = new Hood(new HoodIOSim());
                 // indexer = new Indexer(new IndexerIOSim());
                 climb = new Climb(new ClimbIOSim());
-                // hookprep = new Hookprep(new HookprepIOServo() {});
                 break;
         }
         // autoAim = new AutoAim(turret, hood, flywheel, indexer, drive);
@@ -180,10 +175,6 @@ public class RobotContainer {
         primaryController.b().whileTrue(climb.motorBackward());
         primaryController.x().onTrue(climb.raiseClimbArm());
         primaryController.y().onTrue(climb.raiseRobotLevelOne());
-
-        // primaryController.x().whileTrue(hookprep.readyHook(500));
-        // primaryController.y().whileTrue(hookprep.readyHook(2500));
-        
     }
 
     public Command getAutonomousCommand() {
