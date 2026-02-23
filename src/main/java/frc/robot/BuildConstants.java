111a725ae73bbc869aa2f460cfe37ac1c51d6b67
EmptyDreadbot <mason.hayes@dexterschools.org>
2026-02-23 16:25:39 -0500

    Hood controls

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 148;
-  public static final String GIT_SHA = "7165e4f7f59f9e86770ac903d2a2b017d0af749a";
-  public static final String GIT_DATE = "2026-02-21 15:16:20 EST";
+  public static final int GIT_REVISION = 161;
+  public static final String GIT_SHA = "0b402fd1818d0bb47c53aaf04594682baca2822d";
+  public static final String GIT_DATE = "2026-02-23 10:57:54 EST";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2026-02-21 15:20:56 EST";
-  public static final long BUILD_UNIX_TIME = 1771705256071L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-23 15:46:53 EST";
+  public static final long BUILD_UNIX_TIME = 1771879613434L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
