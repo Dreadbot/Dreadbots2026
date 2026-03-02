cbed98aa620406a182b70413819ca00ee99ac195
EmptyDreadbot <mason.hayes@dexterschools.org>
2026-03-02 17:52:29 -0500

    Setting 1 up / centercenterclimb

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -134,7 +134,7 @@ public class RobotContainer {
         // Set up auto routines
         choreoAutoChooser = new AutoChooser();
         // 1
-        choreoAutoChooser.addRoutine("Center Center Climb", autos::centerCenterClimb);
+        choreoAutoChooser.addRoutine("Center Center Climb", autos::leftCenterCenterClimb);
         // 2
         choreoAutoChooser.addRoutine("Left Center Sweep", autos::leftCenter);
         // 3
