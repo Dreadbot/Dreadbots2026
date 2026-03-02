a1cc8165b0b2e739fdb4d16ee4ee7d7a113f5404
EmptyDreadbot <mason.hayes@dexterschools.org>
2026-03-02 16:30:23 -0500

    CenterCenterClimb pathed

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 233;
-  public static final String GIT_SHA = "b73aeaf99b139721eeb6fec0544fae54c4ba54cc";
-  public static final String GIT_DATE = "2026-03-01 18:57:39 EST";
+  public static final int GIT_REVISION = 236;
+  public static final String GIT_SHA = "c0612a1fa683dc6479138ace78954fe118f4a1c7";
+  public static final String GIT_DATE = "2026-03-02 15:59:07 EST";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2026-03-01 23:54:45 EST";
-  public static final long BUILD_UNIX_TIME = 1772427285270L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-02 16:00:39 EST";
+  public static final long BUILD_UNIX_TIME = 1772485239745L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
