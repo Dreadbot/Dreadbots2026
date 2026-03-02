dde93e14a787bb2f5df9f4629f351626f916d81c
arman3412 <arman12cb@gmail.com>
2026-03-02 17:50:46 -0500

    updated center fire center fire

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 233;
-  public static final String GIT_SHA = "b73aeaf99b139721eeb6fec0544fae54c4ba54cc";
-  public static final String GIT_DATE = "2026-03-01 18:57:39 EST";
+  public static final int GIT_REVISION = 241;
+  public static final String GIT_SHA = "81da4ae4172bd53acc3c0a72e678156def1d93ce";
+  public static final String GIT_DATE = "2026-03-02 16:19:12 EST";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2026-03-01 23:54:45 EST";
-  public static final long BUILD_UNIX_TIME = 1772427285270L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-03-02 16:31:45 EST";
+  public static final long BUILD_UNIX_TIME = 1772487105386L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
