d6bf9e228717a1f1166ebf41205d546acacdc949
jeejDev <156706451+Butter-Stik@users.noreply.github.com>
2026-03-03 18:35:51 -0500

    added build constants to gitignore

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 34;
-  public static final String GIT_SHA = "bfc004a37c571828f495cebca9b6e1cb78cd126c";
-  public static final String GIT_DATE = "2026-02-18 14:55:36 EST";
+  public static final int GIT_REVISION = 35;
+  public static final String GIT_SHA = "68699562574fdaa1ad8fb31f0d1fd8fb325e2dc6";
+  public static final String GIT_DATE = "2026-02-19 20:21:43 EST";
   public static final String GIT_BRANCH = "leds";
-  public static final String BUILD_DATE = "2026-02-19 19:44:00 EST";
-  public static final long BUILD_UNIX_TIME = 1771548240422L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-21 11:14:08 EST";
+  public static final long BUILD_UNIX_TIME = 1771690448366L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
