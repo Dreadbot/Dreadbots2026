dbf17c130abea181f9e82b9d4686e41725e2e398
Mason-1473 <masonthayes@gmail.com>
2026-03-03 17:18:21 -0500

    misc changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 247;
-  public static final String GIT_SHA = "4df6a919e7a0fe75def4583f7a2d866bda6811bf";
-  public static final String GIT_DATE = "2026-03-02 16:40:42 EST";
+  public static final int GIT_REVISION = 256;
+  public static final String GIT_SHA = "12dc75a8f34a59861aa06fda63ba7ec4c8f83d37";
+  public static final String GIT_DATE = "2026-03-02 17:49:24 EST";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2026-03-02 17:14:17 EST";
-  public static final long BUILD_UNIX_TIME = 1772489657219L;
+  public static final String BUILD_DATE = "2026-03-02 18:09:50 EST";
+  public static final long BUILD_UNIX_TIME = 1772492990573L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -18,170 +18,166 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class Climb extends SubsystemBase {
-    // Auto logging output to something
-    private ClimbIO io;
-    private ClimbIOInputsAutoLogged inputs = new ClimbIOInputsAutoLogged();
+        // Auto logging output to something
+        private ClimbIO io;
+        private ClimbIOInputsAutoLogged inputs = new ClimbIOInputsAutoLogged();
 
-    // PID work?
-    private final PIDController pid = new PIDController(ClimbConstants.PIDCONTROLLER_KP, 0.0,
-            ClimbConstants.PIDCONTROLLER_KD);
-    private final TrapezoidProfile profile = new TrapezoidProfile(new TrapezoidProfile.Constraints(
-            ClimbConstants.TRAPEZOID_CONSTRAINTS_MAX_VELOCITY, ClimbConstants.TRAPEZOID_CONSTRAINTS_MAX_ACCELERATION));
-    private TrapezoidProfile.State goal = new TrapezoidProfile.State(ClimbConstants.TRAPEZOID_STATE_POSITION,
-            ClimbConstants.TRAPEZOID_STATE_VELOCITY);
-    private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
-    public final ArmFeedforward feedforward = new ArmFeedforward(ClimbConstants.ARMFEEDFORWARD_KS, 0.0,
-            ClimbConstants.ARMFEEDFORWARD_KV);
-    // private DigitalInput lowerSwitch = new
-    // DigitalInput(ClimbConstants.LOWER_LIMIT_SWITCH_ID);
-    // private DigitalInput upperSwitch = new
-    // DigitalInput(ClimbConstants.UPPER_LIMIT_SWITCH_ID);
+        // PID work?
+        private final PIDController pid = new PIDController(ClimbConstants.PIDCONTROLLER_KP, 0.0,
+                        ClimbConstants.PIDCONTROLLER_KD);
+        private final TrapezoidProfile profile = new TrapezoidProfile(new TrapezoidProfile.Constraints(
+                        ClimbConstants.TRAPEZOID_CONSTRAINTS_MAX_VELOCITY,
+                        ClimbConstants.TRAPEZOID_CONSTRAINTS_MAX_ACCELERATION));
+        private TrapezoidProfile.State goal = new TrapezoidProfile.State(ClimbConstants.TRAPEZOID_STATE_POSITION,
+                        ClimbConstants.TRAPEZOID_STATE_VELOCITY);
+        private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
+        public final ArmFeedforward feedforward = new ArmFeedforward(ClimbConstants.ARMFEEDFORWARD_KS, 0.0,
+                        ClimbConstants.ARMFEEDFORWARD_KV);
+        private DigitalInput lowerSwitch = new DigitalInput(ClimbConstants.LOWER_LIMIT_SWITCH_ID);
+        private DigitalInput upperSwitch = new DigitalInput(ClimbConstants.UPPER_LIMIT_SWITCH_ID);
 
-    @AutoLogOutput
-    // Setting up the boolean Varible, which is for right now isClimbed (Basic will
-    // be updated later)
-    public boolean isClimbed = false;
-    public boolean raisingArm = false;
-    public boolean loweringArm = false;
-    public boolean climbing = false;
+        @AutoLogOutput
+        // Setting up the boolean Varible, which is for right now isClimbed (Basic will
+        // be updated later)
+        public boolean isClimbed = false;
+        public boolean raisingArm = false;
+        public boolean loweringArm = false;
+        public boolean climbing = false;
 
-    private Slapdown slapdown;
+        private Slapdown slapdown;
 
-    public Climb(ClimbIO io, Slapdown slapdown) {
-        this.io = io;
-        this.slapdown = slapdown;
-    }
+        public Climb(ClimbIO io, Slapdown slapdown) {
+                this.io = io;
+                this.slapdown = slapdown;
+        }
 
-    // public Command motorForward() {
-    // return Commands.startEnd(
-    // () -> {
-    // if (upperSwitch.get()) raisingArm = true;
-    // },
-    // () -> raisingArm = false
-    // );
-    // }
+        public Command motorForward() {
+                return Commands.startEnd(
+                                () -> {
+                                        if (upperSwitch.get())
+                                                raisingArm = true;
+                                },
+                                () -> raisingArm = false);
+        }
 
-    // public Command motorBackward() {
-    // return Commands.startEnd(
-    // () -> {
-    // if (lowerSwitch.get()) loweringArm = true;
-    // },
-    // () -> loweringArm = false
-    // );
-    // }
+        public Command motorBackward() {
+                return Commands.startEnd(
+                                () -> {
+                                        if (lowerSwitch.get())
+                                                loweringArm = true;
+                                },
+                                () -> loweringArm = false);
+        }
 
-    // public Command raiseClimbArm() {
-    // return Commands.runOnce(() -> {
-    // if (upperSwitch.get()) raisingArm = true;
-    // });
-    // }
+        public Command raiseClimbArm() {
+                return Commands.runOnce(() -> {
+                        if (upperSwitch.get())
+                                raisingArm = true;
+                });
+        }
 
-    // public Command raiseRobotLevelOne() {
-    // return Commands.runOnce (() -> {
-    // if (lowerSwitch.get()) goal = new
-    // TrapezoidProfile.State(ClimbConstants.LEVEL_ONE_CLIMB_POSITION, 0);
-    // });
-    // }
+        public Command levelOneClimb() {
+                return Commands.sequence(
+                                raiseClimbArm(),
+                                Commands.waitUntil(() -> raisingArm == false),
+                                raiseRobotLevelOne());
+        }
 
-    // public Command levelOneClimb() {
-    // return Commands.sequence(
-    // raiseClimbArm(),
-    // Commands.waitUntil(() -> raisingArm == false),
-    // raiseRobotLevelOne()
-    // );
-    // }
+        public Command lowerClimbArm() {
+                return Commands.runOnce(() -> {
+                        if (lowerSwitch.get())
+                                loweringArm = true;
+                });
+        }
 
-    // public Command lowerClimbArm() {
-    // return Commands.runOnce(() -> {
-    // if (lowerSwitch.get()) loweringArm = true;
-    // });
-    // }
+        public Command raiseRobotLevelOne() {
+                return Commands.runOnce(() -> {
+                        if (lowerSwitch.get())
+                                goal = new TrapezoidProfile.State(ClimbConstants.LEVEL_ONE_CLIMB_POSITION, 0);
+                });
+        }
 
-    // public Command raiseRobotLevelOne() {
-    // return Commands.runOnce (() -> {
-    // if (lowerSwitch.get()) goal = new
-    // TrapezoidProfile.State(ClimbConstants.LEVEL_ONE_CLIMB_POSITION, 0);
-    // });
-    // }
+        public Command climb() {
+                return Commands.runOnce(() -> {
+                        climbing = !climbing;
+                        if (climbing)
+                                raisingArm = true;
+                        else
+                                raisingArm = false;
+                        loweringArm = false;
+                });
+        }
 
-    // public Command climb() {
-    // return Commands.runOnce(() -> {
-    // climbing = !climbing;
-    // if (climbing) raisingArm = true;
-    // else raisingArm = false;
-    // loweringArm = false;
-    // });
-    // }
+        public Command testLower() {
+                return Commands.startEnd(
+                                () -> io.runVoltage(ClimbConstants.LOWER_VOLTAGE),
+                                () -> io.runVoltage(0.0),
+                                this);
 
-    public Command testLower() {
-        return Commands.startEnd(
-                () -> io.runVoltage(ClimbConstants.LOWER_VOLTAGE),
-                () -> io.runVoltage(0.0),
-                this);
+        }
 
-    }
+        public Command testRaise() {
+                return Commands.startEnd(
+                                () -> io.runVoltage(ClimbConstants.RAISE_VOLTAGE),
+                                () -> io.runVoltage(0.0),
+                                this);
+        }
 
-    public Command testRaise() {
-        return Commands.startEnd(
-                () -> io.runVoltage(ClimbConstants.RAISE_VOLTAGE),
-                () -> io.runVoltage(0.0),
-                this);
-    }
-    
-    // public Command extentionChecker() {
-    //     return Commands.runOnce(() -> {
-    //         io.isSlapdownExtended();
-    //     });
-    // }
+        public Command extentionChecker() {
+                return Commands.runOnce(() -> {
+                        io.isSlapdownExtended();
+                });
+        }
 
-    
-    // Updates the inputs of ClimbIO perodic.
-    // ClimbIO takes the inputs and outputs of Climb from the contorller
-    // @Override
-    // public void periodic() {
+        // Updates the inputs of ClimbIO perodic.
+        // ClimbIO takes the inputs and outputs of Climb from the contorller
+        @Override
+        public void periodic() {
 
-//         io.updateInputs(inputs);
-//         Logger.processInputs("Climb", inputs);
-//         if (DriverStation.isDisabled()) {
-//             setpoint = new TrapezoidProfile.State(inputs.absolutePosition, 0);
-//             goal = setpoint;
-//         }
+                io.updateInputs(inputs);
+                Logger.processInputs("Climb", inputs);
+                if (DriverStation.isDisabled()) {
+                        setpoint = new TrapezoidProfile.State(inputs.absolutePosition, 0);
+                        goal = setpoint;
+                }
 
-//         Logger.recordOutput("Climb/SetpointPosition", setpoint.position);
-//         Logger.recordOutput("Climb/GoalPosition", goal.position);
-//         Logger.recordOutput("Climb/LowerSwitch", lowerSwitch.get());
-//         Logger.recordOutput("Climb/UpperSwitch", upperSwitch.get());
-//         Logger.recordOutput("Climb/RaisingArm", raisingArm);
-//         Logger.recordOutput("Climb/LoweringArm", loweringArm);
-//         setpoint = profile.calculate(0.02, setpoint, goal);
+                Logger.recordOutput("Climb/SetpointPosition", setpoint.position);
+                Logger.recordOutput("Climb/GoalPosition", goal.position);
+                Logger.recordOutput("Climb/LowerSwitch", lowerSwitch.get());
+                Logger.recordOutput("Climb/UpperSwitch", upperSwitch.get());
+                Logger.recordOutput("Climb/RaisingArm", raisingArm);
+                Logger.recordOutput("Climb/LoweringArm", loweringArm);
+                setpoint = profile.calculate(0.02, setpoint, goal);
 
-//         // If upperSwitch is tripped
-//         if (!upperSwitch.get()) {
-//             if (climbing) loweringArm = true;
-//             raisingArm = false;
-//             io.setPosition(0);
-//             goal = new TrapezoidProfile.State(setpoint.position, 0);
-//         }
-//         // If lowerSwitch is tripped
-//         if (!lowerSwitch.get()) {
-//             if (climbing) raisingArm = true;
-//             loweringArm = false;
-//             goal = new TrapezoidProfile.State(setpoint.position, 0);
-//         }
+                // If upperSwitch is tripped
+                if (!upperSwitch.get()) {
+                        if (climbing)
+                                loweringArm = true;
+                        raisingArm = false;
+                        io.setPosition(0);
+                        goal = new TrapezoidProfile.State(setpoint.position, 0);
+                }
+                // If lowerSwitch is tripped
+                if (!lowerSwitch.get()) {
+                        if (climbing)
+                                raisingArm = true;
+                        loweringArm = false;
+                        goal = new TrapezoidProfile.State(setpoint.position, 0);
+                }
 
-//         if (raisingArm) {
-//             io.runVoltage(ClimbConstants.RAISE_VOLTAGE);
-//             return;
-//         }
-//         if (loweringArm) {
-//             io.runVoltage(ClimbConstants.LOWER_VOLTAGE);
-//             return;
-//         }
-//         io.runVoltage(
-//             pid.calculate(inputs.absolutePosition, setpoint.position) +
-//             feedforward.calculate(inputs.absolutePosition + 90, setpoint.velocity)
-//             // use acutal position degrees to make sure that we always apply the correct
-//             // gravity feed forward.
-//         );
-//     }
+                if (raisingArm) {
+                        io.runVoltage(ClimbConstants.RAISE_VOLTAGE);
+                        return;
+                }
+                if (loweringArm) {
+                        io.runVoltage(ClimbConstants.LOWER_VOLTAGE);
+                        return;
+                }
+                io.runVoltage(
+                                pid.calculate(inputs.absolutePosition, setpoint.position) +
+                                                feedforward.calculate(inputs.absolutePosition + 90, setpoint.velocity)
+                // use acutal position degrees to make sure that we always apply the correct
+                // gravity feed forward.
+                );
+        }
 }
