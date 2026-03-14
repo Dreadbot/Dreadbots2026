118b88c840ad8a42e5558aefd6aac46801fedd1d
Mason-1473 <masonthayes@gmail.com>
2026-03-14 12:20:19 -0400

    added logic to the slapdown profiles

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Dreadbots2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 304;
-  public static final String GIT_SHA = "c75fff84778ae680925a283f48212bdec9808b08";
-  public static final String GIT_DATE = "2026-03-12 20:35:24 EDT";
+  public static final int GIT_REVISION = 307;
+  public static final String GIT_SHA = "f4fee81aa448ce46dbdb6cb49c4204827685c6fd";
+  public static final String GIT_DATE = "2026-03-14 11:53:12 EDT";
   public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2026-03-13 18:38:05 EDT";
-  public static final long BUILD_UNIX_TIME = 1773441485905L;
+  public static final String BUILD_DATE = "2026-03-14 12:13:43 EDT";
+  public static final long BUILD_UNIX_TIME = 1773504823191L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
